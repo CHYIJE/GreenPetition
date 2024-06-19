8b3e86aa447fab3b08d7fabd73b78217e23494f5
ryipuxt <happy201732@gmail.com>
2024-06-19 17:06:53 +0900

    complete id duplication

--- src/Frame/SigninFrame.java
+++ src/Frame/SigninFrame.java
@@ -12,97 +12,118 @@ import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 import lombok.Getter;
+import ver1.ObjectDAO.DuplicationDAO;
 import ver1.ObjectDAO.JoinDAO;
 import ver1.models.UserDTO;
 
 @Getter
-public class SigninFrame extends JFrame{
-	
-	JoinDAO dao;
+public class SigninFrame extends JFrame {
+
+	JoinDAO joindao;
+	DuplicationDAO duplicationdao;
 	UserDTO dto;
-	
+
 	SigninFrame mContext = this;
-	
+
 	private JLabel frame;
 	private JTextField idField;
 	private JTextField pwField;
 	private JTextField nameField;
 	private JButton signIn;
+	private JButton duplication;
 
 	public SigninFrame() {
 		initData();
 		setInitLayout();
 		addAction();
 	}
-		
+
 	public void initData() {
-		
+
 		setTitle("Sign-In");
 		frame = new JLabel(new ImageIcon("img/signinFrame.png"));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setContentPane(frame);
 		setSize(1280, 900);
-		
+
 		mContext.idField = new JTextField();
 		idField.setBorder(javax.swing.BorderFactory.createEmptyBorder());
 		idField.setBounds(500, 270, 260, 30);
-		
+
 		mContext.pwField = new JTextField();
 		pwField.setBorder(javax.swing.BorderFactory.createEmptyBorder());
 		pwField.setBounds(500, 460, 260, 30);
-		
+
 		mContext.nameField = new JTextField();
 		nameField.setBorder(javax.swing.BorderFactory.createEmptyBorder());
 		nameField.setBounds(500, 640, 260, 30);
-		
+
 		signIn = new JButton(new ImageIcon("img/signinButton190_68.png"));
 		signIn.setBounds(520, 770, 220, 80);
 		signIn.setBorderPainted(false);
-		signIn.setBackground(new Color(255,255,255));
-		
+		signIn.setBackground(new Color(255, 255, 255));
+
+		duplication = new JButton(new ImageIcon("img/duplicationButton.png"));
+		duplication.setBounds(780, 250, 280, 80);
+		duplication.setBorderPainted(false);
+		duplication.setBackground(new Color(255, 255, 255));
+
 	}
+
 	public void setInitLayout() {
 		setLayout(null);
 		setResizable(false);
 		setLocationRelativeTo(null);
-		
+
 		add(idField);
 		add(pwField);
 		add(nameField);
 		add(signIn);
-		
+		add(duplication);
+
 		setVisible(true);
 	}
-	
+
 	public void addAction() {
 		signIn.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
-				if(!mContext.idField.getText().equals("") && !mContext.pwField.getText().equals("") && !mContext.nameField.getText().equals("")) {
-						try {
-							dao = new JoinDAO(dto, mContext); 
-						} catch (Exception e1) {
-							e1.printStackTrace();
-						}
-					
-						
-						JOptionPane.showMessageDialog(null, "회원가입이 완료되었습니다.");
-						dispose();
-						
-					
+
+				if (!mContext.idField.getText().equals("") && !mContext.pwField.getText().equals("")
+						&& !mContext.nameField.getText().equals("")) {
+					try {
+						joindao = new JoinDAO(dto, mContext);
+					} catch (Exception e1) {
+						e1.printStackTrace();
+					}
+					dispose();
 				} else {
 					JOptionPane.showMessageDialog(null, "ID/PW/NAME 입력해주세요.");
 				}
 			}
-			
+
+		});
+		duplication.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (!mContext.idField.getText().equals("")) {
+					try {
+						duplicationdao = new DuplicationDAO(dto, mContext);
+					} catch (Exception e2) {
+						// TODO: handle exception
+					}
+				} else {
+					JOptionPane.showMessageDialog(null, "ID 입력해주세요.");
+				}
+			}
 		});
 
-		
 	}
+
 	public static void main(String[] args) {
 		new SigninFrame();
 	}
-	
+
 }
--- src/ver1/ObjectDAO/JoinDAO.java
+++ src/ver1/ObjectDAO/JoinDAO.java
@@ -2,10 +2,9 @@ package ver1.ObjectDAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+
+import javax.swing.JOptionPane;
 
 import Frame.SigninFrame;
 import ver1.DBConnectionManager;
@@ -33,12 +32,10 @@ public class JoinDAO {
 			e.printStackTrace();
 		}
 	}
-
-<<<<<<< HEAD
-	public void joinUser(UserDTO dto, SigninFrame mContext) {
-=======
+	
+	// 회원가입
 	public void joinUser(UserDTO dto, SigninFrame mContext) throws SQLException {
->>>>>>> 3e2dde5a7a1f1aa14fbc3591f757a6f13a019978
+
 		this.mContext = mContext;
 
 		String insertQuery = "INSERT INTO user(name, acc_id, acc_pw) values (? ,?, ?) ";
@@ -47,37 +44,21 @@ public class JoinDAO {
 
 			conn.setAutoCommit(false);
 
-			List<UserDTO> idlist = new ArrayList<>();
-			String checkIdQuery = "SELECT acc_id FROM user ";
-			PreparedStatement selectptmt = conn.prepareStatement(checkIdQuery);
-			ResultSet rs = selectptmt.executeQuery();
+			PreparedStatement insertptmt = conn.prepareStatement(insertQuery);
+			insertptmt.setString(1, mContext.getIdField().getText());
+			insertptmt.setString(2, mContext.getPwField().getText());
+			insertptmt.setString(3, mContext.getNameField().getText());
 
-			while (rs.next()) {
-				UserDTO dto1 = new UserDTO().builder().acc_id(rs.getString("acc_id")).build();
-				idlist.add(dto1);
-				System.out.println("while 들어오긴 함");
-				if (idlist.equals(mContext.getIdField().getText())) {
-					PreparedStatement insertptmt = conn.prepareStatement(insertQuery);
-					insertptmt.setString(1, mContext.getIdField().getText());
-					insertptmt.setString(2, mContext.getPwField().getText());
-					insertptmt.setString(3, mContext.getNameField().getText());
+			int rowCount = insertptmt.executeUpdate();
 
-					System.out.println(rs.getString("acc_id"));
-					int testRow = insertptmt.executeUpdate();
-
-					conn.commit();
-					System.out.println(testRow);
-				} else {
-					conn.rollback();
-				}
-<<<<<<< HEAD
-				System.out.println("break 처리 완료");
-				break;
+			if(rowCount > 0) {
+				conn.commit();
+				JOptionPane.showMessageDialog(null, "회원가입이 완료되었습니다.");
+			} else {
+				conn.rollback();
 			}
 
-		} catch (SQLException e) {
-=======
->>>>>>> 3e2dde5a7a1f1aa14fbc3591f757a6f13a019978
+			System.out.println("break 처리 완료");
 
 		}
 	}
