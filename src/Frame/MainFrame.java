9ae0f39e24a438902e83dc91f0ea06227a3b1ae2
HyeonProG <guswo3443@naver.com>
2024-06-19 15:05:37 +0900

    ?

--- src/Frame/MainFrame.java
+++ src/Frame/MainFrame.java
@@ -4,12 +4,17 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
+
+import ver1.DBConnectionManager;
 
 public class MainFrame extends JFrame{
 	
--- src/ver1/ObjectDAO/JoinDAO.java
+++ src/ver1/ObjectDAO/JoinDAO.java
@@ -2,6 +2,7 @@ package ver1.ObjectDAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import Frame.SigninFrame;
@@ -11,15 +12,14 @@ import ver1.models.UserDTO;
 public class JoinDAO {
 	SigninFrame mContext;
 
-	
 	public void setContext(SigninFrame context) {
-        this.mContext = context;
-    }
-	
+		this.mContext = context;
+	}
+
 	UserDTO dto;
 
 	public JoinDAO(UserDTO dto, SigninFrame mContext) {
-		
+
 		try {
 			joinUser(dto, mContext);
 			this.mContext = mContext;
@@ -27,22 +27,31 @@ public class JoinDAO {
 			e.printStackTrace();
 		}
 	}
-	
-	public void joinUser(UserDTO dto, SigninFrame mContext) throws SQLException{
+
+	public void joinUser(UserDTO dto, SigninFrame mContext) throws SQLException {
 		this.mContext = mContext;
-		
+		String checkIdQuery = "SELECT acc_id From user";
 		String insertQuery = "INSERT INTO user(name, acc_id, acc_pw) values (? ,?, ?) ";
 
-		try (Connection conn = DBConnectionManager.getInstance().getConnection()){
+		try (Connection conn = DBConnectionManager.getInstance().getConnection()) {
 			PreparedStatement ptmt = conn.prepareStatement(insertQuery);
 			ptmt.setString(1, mContext.getIdField().getText());
 			ptmt.setString(2, mContext.getPwField().getText());
 			ptmt.setString(3, mContext.getNameField().getText());
-			
 
 			int testRow = ptmt.executeUpdate();
 			System.out.println(testRow);
 
+		}
+		
+		
+		try (Connection conn = DBConnectionManager.getInstance().getConnection()) {
+			PreparedStatement ptmt = conn.prepareStatement(checkIdQuery);
+			
+			ResultSet rs = ptmt.executeQuery();
+			rs = 
+					
+			
 		}
 	}
 }
