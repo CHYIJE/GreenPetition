a046ebbf17907ff17ad07a4b93375dbfafb12f1f
spglados <spglados@gmail.com>
2024-06-19 14:36:22 +0900

    aaaa

--- src/ver1/models/UserDTO.java
+++ src/ver1/models/UserDTO.java
@@ -1,7 +1,7 @@
 package ver1.models;
 
-<<<<<<< HEAD
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,15 +12,9 @@ import lombok.ToString;
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class UserDTO {
 	private String name;
 	private String acc_id;
 	private String acc_pw;
-=======
-public class UserDTO {
-	private int id;
-	private String 회원이름;
-	private String 아이디;
-	private String 비번;
->>>>>>> e6b776f635e02e46e1846d5ca5d609be4bef3b5e
 }
