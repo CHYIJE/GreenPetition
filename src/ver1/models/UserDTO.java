226fb3d0509a8471251760ac60ebd3edad9112f5
HyeonProG <guswo3443@naver.com>
2024-06-20 11:21:50 +0900

    writer

--- src/ver1/models/UserDTO.java
+++ src/ver1/models/UserDTO.java
@@ -14,6 +14,7 @@ import lombok.ToString;
 @NoArgsConstructor
 @Builder
 public class UserDTO {
+	private int id;
 	private String name;
 	private String acc_id;
 	private String acc_pw;
--- src/ver1/models/patitionDTO.java
+++ src/ver1/models/patitionDTO.java
@@ -1,5 +1,19 @@
 package ver1.models;
 
-public class patitionDTO {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class patitionDTO {
+	private int id;
+	private int user_id;
+	private Category category;
+	
 }
