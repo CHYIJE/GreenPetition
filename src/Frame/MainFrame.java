f85961d7c7cf6408db2221110966dbabc0ad1f83
LOSTANA <jyj970602@naver.com>
2024-06-25 15:48:28 +0900

    검색수정

--- src/Frame/MainFrame.java
+++ src/Frame/MainFrame.java
@@ -331,9 +331,7 @@ public class MainFrame extends JFrame {
 
 					int id = (int) table.getValueAt(row, 0);
 
-
-				getValueFromDatabase(id);
-
+					getValueFromDatabase(id);
 
 					new CheckerFrame(currentUser, id, mcontext);
 				}
@@ -390,11 +388,43 @@ public class MainFrame extends JFrame {
 
 		model.setRowCount(0);
 		if (teacher == true) {
-			table.setModel(tDao.insertData().getModel());
+
+			if (search == true) {
+
+				String searchTerm = searchField.getText();
+				List<PatitionDTO> searchResults = searchDAO.titleSearch(searchTerm);
+				model.setRowCount(0); // 기존 데이터 초기화
+				for (PatitionDTO result : searchResults) {
+					if (result.getCategory().toString() == "teacher") {
+						model.addRow(new Object[] { result.getId(), result.getTitle(), result.getAcc_id(),
+								result.getCategory(), result.getAgree(), result.getDisagree(), result.getDate() });
+					} else {
+						continue;
+					}
+				}
+			} else {
+				table.setModel(tDao.insertData().getModel());
+			}
 			tableset();
 		} else if (facility == true) {
 
-			table.setModel(fDao.insertData().getModel());
+			if (search == true) {
+				String searchTerm = searchField.getText();
+				List<PatitionDTO> searchResults = searchDAO.titleSearch(searchTerm);
+				model.setRowCount(0); // 기존 데이터 초기화
+
+				for (PatitionDTO result : searchResults) {
+					if (result.getCategory().toString() == "facility") {
+						model.addRow(new Object[] { result.getId(), result.getTitle(), result.getAcc_id(),
+								result.getCategory(), result.getAgree(), result.getDisagree(), result.getDate() });
+					} else {
+						continue;
+					}
+				}
+
+			}else {
+				table.setModel(fDao.insertData().getModel());
+			}
 			tableset();
 		} else if (search == true) {
 			String searchTerm = searchField.getText();
@@ -408,7 +438,7 @@ public class MainFrame extends JFrame {
 		}
 
 		else {
-
+			table.setModel(article.insertData().getModel());
 			tableset();
 
 		}
